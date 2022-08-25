913462e4e734a123994994493d6db5285151bcd2
Tyler Garner <garnertb@github.com>
2022-08-25 16:59:54 -0400

    wip

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const handlebars = require('handlebars');
 const core = require('@actions/core');
 const yaml = require("js-yaml"); 
 
+handlebars.registerHelper('withinAWeek', function(arg1, opts) {
+  return (Date.now() - Date.parse(arg1) < 86400000 * 7) ? opts.fn(this) : opts.inverse(this);
+});
+
 async function main() {
   const template = core.getInput('TEMPLATE', {required: true, trimWhitespace: true})
   const compiledTemplate = handlebars.compile(template);
@@ -11,7 +15,7 @@ async function main() {
   
   core.debug('inputs', JSON.stringify(inputs,undefined,2));
   const result = compiledTemplate(inputs)
-  core.debug(`compliled results: ${result}`)
+  core.debug(`compiled results: ${result}`)
   core.setOutput('payload', result)
   return result;
 }
@@ -20,9 +24,12 @@ function getAllInputs() {
   return Object.entries(process.env).reduce((result, [key, value]) => {
     if (!/^INPUT_/.test(key)) return result;
     const inputName = key.substr("INPUT_".length).toLowerCase();
+    
     core.debug(`reading input key: ${key}, value: ${value}`);
-    result[inputName] = yaml.load(value);
-
+    
+    if (key !== 'INPUT_TEMPLATE') {
+      result[inputName] = yaml.load(value);
+    }
     return result;
   }, {})
 };
