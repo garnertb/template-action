31bc7824dbef7e72af7f4105e8d349a3de6bec5f
Tyler Garner <garnertb@github.com>
2022-08-25 13:54:56 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const fs = require('fs').promises;
+const handlebars = require('handlebars');
+const core = require('@actions/core');
+const yaml = require("js-yaml"); 
+
+async function main() {
+  const template = core.getInput('TEMPLATE', {required: true, trimWhitespace: true})
+  const compiledTemplate = handlebars.compile(template);
+  const inputs = getAllInputs();
+  
+  core.debug('inputs', JSON.stringify(inputs,undefined,2));
+  const result = compiledTemplate(inputs)
+  core.debug(`compliled results: ${result}`)
+  return result;
+}
+
+function getAllInputs() {
+  return Object.entries(process.env).reduce((result, [key, value]) => {
+    if (!/^INPUT_/.test(key)) return result;
+    const inputName = key.substr("INPUT_".length).toLowerCase();
+    result[inputName] = yaml.load(value);
+
+    return result;
+  }, {})
+};
+
+main()
